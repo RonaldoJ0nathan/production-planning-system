ba2ef6b0f5f01072a36e76bcbb315b64a17cf339
Ronaldo Jonathan <ronaldoj.dev@gmail.com>
2026-02-24 10:02:43 -0400

    chore(frontend): disable allowCypressEnv to fix config security warning

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  allowCypressEnv: false,
   e2e: {
     baseUrl: 'http://localhost',
     supportFile: 'cypress/support/e2e.ts',
